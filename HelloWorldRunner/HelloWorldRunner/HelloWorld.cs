8aefd009b60e9128bcbe52afa0da39ce3568445d
The_Lump <thelump@lump.com>
2021-06-25 11:01:57 -0400

    Hello world finished

---
+++ HelloWorldRunner/HelloWorldRunner/HelloWorld.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorldRunner
+{
+    public class HelloWorld
+    {
+        private string message = "Hello World";
+
+        public void SayHello()
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
---
+++ HelloWorldRunner/HelloWorldRunner/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HelloWorldRunner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            HelloWorld hello = new HelloWorld();
+            hello.SayHello();
+        }
+    }
+}
